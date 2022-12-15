43924b26597ec87329fbbef92351cd016cc65204
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-12-15 15:37:04 -0500

    all routes work

--- controllers/book.js
+++ controllers/book.js
@@ -14,4 +14,26 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.put('/:id', async(req, res) =>{
+    try{
+        const updatedBook = await Book.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+        res.json(updatedBook)
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try{
+        const deletedBook = await Book.findByIdANDRemove(req.params.id)
+        res.json(deletedBook)
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
 module.exports = router
